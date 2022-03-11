6214fba5af0f2a51322b97df8df11f6a0f7b6a28
ivanjonatas <42282086+ivanjonatas@users.noreply.github.com>
2022-03-10 23:01:05 -0300

    update variable.td
    retirando o description

--- variable.tf
+++ variable.tf
@@ -1,14 +1,11 @@
 variable "name"{
-    description = "xxx"
     type = string
 }
 
 variable "location"{
-    description = "xxx"
     type = string
 }
 
 variable "destroy"{
-    description = "xxx"
     type = bool
 }
